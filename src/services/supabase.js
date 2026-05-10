e157c193c9d89f21b2f9ffbe6f1d4b0c8935be6b
Claudio Chaves Ferreira <petrio250@gmail.com>
2026-05-10 16:58:32 -0300

    meu site

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || ''
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || ''
+const supabaseUrl = 'https://dwyzycovqccfbucovqzt.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3eXp5Y292cWNjZmJ1Y292cXp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0MjgyNTcsImV4cCI6MjA5NDAwNDI1N30.L7TWKXkB6laud7HMvP-2JPgJyCvjE8AvF18pAKm3xQw'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
