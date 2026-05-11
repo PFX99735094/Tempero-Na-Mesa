41810b10ec1a4b8fc8b02b18dccae55e535d6d94
Claudio Chaves Ferreira <petrio250@gmail.com>
2026-05-10 21:00:40 -0300

    meu site

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -57,8 +57,6 @@ export default function Layout() {
         
         <Outlet />
       </div>
-
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     </div>
   )
 }
